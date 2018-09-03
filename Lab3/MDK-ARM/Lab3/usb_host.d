030e08f1b28c58d0071884e8b706f6ae368ccb18
Pongsathorn Kerdphol <aqover@hotmail.com>
2018-09-04 01:06:00 +0700

    [V1.0]

---
+++ Lab3/MDK-ARM/Lab3/usb_host.d
@@ -0,0 +1,50 @@
+lab3\usb_host.o: ../Src/usb_host.c
+lab3\usb_host.o: ../Inc/usb_host.h
+lab3\usb_host.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+lab3\usb_host.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f407xx.h
+lab3\usb_host.o: ../Drivers/CMSIS/Include/core_cm4.h
+lab3\usb_host.o: D:\Program Files\ARM\ARMCC\Bin\..\include\stdint.h
+lab3\usb_host.o: ../Drivers/CMSIS/Include/core_cmInstr.h
+lab3\usb_host.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
+lab3\usb_host.o: ../Drivers/CMSIS/Include/core_cmFunc.h
+lab3\usb_host.o: ../Drivers/CMSIS/Include/core_cmSimd.h
+lab3\usb_host.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h
+lab3\usb_host.o: ../Inc/stm32f4xx_hal_conf.h
+lab3\usb_host.o: ../Inc/main.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h
+lab3\usb_host.o: ../Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
+lab3\usb_host.o: D:\Program Files\ARM\ARMCC\Bin\..\include\stdio.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h
+lab3\usb_host.o: ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h
+lab3\usb_host.o: ../Inc/usbh_conf.h
+lab3\usb_host.o: D:\Program Files\ARM\ARMCC\Bin\..\include\stdlib.h
+lab3\usb_host.o: D:\Program Files\ARM\ARMCC\Bin\..\include\string.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_def.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ioreq.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_core.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_pipes.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Core/Inc/usbh_ctlreq.h
+lab3\usb_host.o: ../Middlewares/ST/STM32_USB_Host_Library/Class/CDC/Inc/usbh_cdc.h
---
+++ README.md
@@ -0,0 +1,1 @@
+use toolchin MDK-ARM V5
